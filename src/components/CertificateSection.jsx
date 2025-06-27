2977b05cb53dcd24f409bbef96d66e655550efb9
DimasAswito <dimasaswito@gmail.com>
2025-06-27 19:03:17 +0700

    Update CertificateSection.jsx

--- src/components/CertificateSection.jsx
+++ src/components/CertificateSection.jsx
@@ -6,10 +6,8 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.mjs`;
+
 
 const CertificateDisplay = ({ imageUrl, altText }) => {
   if (!imageUrl) {
